288d4ede0a0505cf03022eec7b508b4c10e18128
to-ql <reverofyoung@gmail.com>
2021-05-02 16:51:56 +0900

    project add

--- js/workPage.js
+++ js/workPage.js
@@ -4,6 +4,8 @@ const workBtn02 = document.querySelector(".js-workBtn02")
 const workPage02 = document.querySelector(".js-workPage02")
 const workBtn03 = document.querySelector(".js-workBtn03")
 const workPage03 = document.querySelector(".js-workPage03")
+const workBtn04 = document.querySelector(".js-workBtn04")
+const workPage04 = document.querySelector(".js-workPage04")
 
 const WORK_CLICKED = "workAni"
 
@@ -11,26 +13,35 @@ function handleClickWork01() {
     workPage01.classList.add(WORK_CLICKED); 
     workPage02.classList.remove(WORK_CLICKED);
     workPage03.classList.remove(WORK_CLICKED);  
+    workPage04.classList.remove(WORK_CLICKED); 
  }
 
  function handleClickWork02() {
     workPage02.classList.add(WORK_CLICKED); 
     workPage01.classList.remove(WORK_CLICKED); 
     workPage03.classList.remove(WORK_CLICKED); 
-    
+    workPage04.classList.remove(WORK_CLICKED); 
  }
 
  function handleClickWork03() {
     workPage03.classList.add(WORK_CLICKED); 
     workPage01.classList.remove(WORK_CLICKED); 
     workPage02.classList.remove(WORK_CLICKED); 
+    workPage04.classList.remove(WORK_CLICKED); 
  }
+ function handleClickWork04() {
+   workPage04.classList.add(WORK_CLICKED); 
+   workPage01.classList.remove(WORK_CLICKED); 
+   workPage02.classList.remove(WORK_CLICKED); 
+   workPage03.classList.remove(WORK_CLICKED); 
+}
 
 
 function init () {
     workBtn01.addEventListener("click", handleClickWork01)
     workBtn02.addEventListener("click", handleClickWork02)
     workBtn03.addEventListener("click", handleClickWork03)
+    workBtn04.addEventListener("click", handleClickWork04)
 }
 
 init();
